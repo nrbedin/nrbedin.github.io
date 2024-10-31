418ae2b9af6b31223a99132132846ffe3674c746
nrbedin <nrbedin@gmail.com>
2024-10-31 11:12:58 -0300

    fix:review deploy with action

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b6bfba9f5bd26d5d27e1e03baec7e556
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "44506c89e5dfb3f9889fdd58d403c525",
+"flutter_bootstrap.js": "410335bbdaffdb93929b5778453184d7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
