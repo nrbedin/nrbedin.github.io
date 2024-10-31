6fa575872109e41efe7aa6604880396373392f2a
nrbedin <nrbedin@gmail.com>
2024-10-31 00:43:27 -0300

    feat: deploy pages git

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b6bfba9f5bd26d5d27e1e03baec7e556
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f4e91617b00819bcbd73fdc3bff72887",
+"flutter_bootstrap.js": "c50e5e6534b324a5569a3aac3db1f8ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
