79b670e09e668fb52fa503592066564e9ba5035f
nrbedin <nrbedin@gmail.com>
2024-11-01 13:27:01 -0300

    fix: update description projects

--- lib/pages/projects/projects_page.dart
+++ lib/pages/projects/projects_page.dart
@@ -60,15 +60,6 @@ class ProjectsPage extends StatelessWidget {
                           ),
                           Padding(
                             padding: const EdgeInsets.all(10.0),
-                            child: Text(
-                              "Data de Desenvolvimento: ${projeto.data}",
-                              style: const TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
                             child: Row(
                               children: [
                                 Expanded(
@@ -107,13 +98,12 @@ class ProjectsPage extends StatelessWidget {
 
 class Projeto {
   final String nome;
-  final String data;
+
   final String descricao;
   final String githubLink;
 
   Projeto({
     required this.nome,
-    required this.data,
     required this.descricao,
     required this.githubLink,
   });
@@ -121,22 +111,47 @@ class Projeto {
 
 final List<Projeto> projetos = [
   Projeto(
-    nome: "Aplicativo Delivery - Flutter web",
-    data: "Setembro de 2023",
+    nome: "Delivery Bida - Flutter WEB",
     descricao:
-        "Desenvolvimento em Dart, usando a framework Flutter, um aplicativo que realiza o controle dos pedidos de delivery",
+        '''Delivery Bida é um sistema de backoffice desenvolvido em Flutter WEB, utilizando arquitetura MVC com Design Pattern Repository e Service,
+e os pacotes Modular e MobX.
+O desenvolvimento do Delivery Bida envolveu a criação de um sistema, capaz de gerenciar pedidos de delivery de maneira integrada e prática.
+           Utilizando o poder do Flutter e pacotes modernos como Modular e MobX, o sistema garante uma performance excepcional e uma interface intuitiva.\n
+              - Gestão de pedidos delivery;\n
+              - Monitoramento de pedidos em tempo real;\n
+              - Interface intuitiva e responsiva para uma experiência otimizada;\n''',
     githubLink: "https://github.com/nrbedin/deliverybida",
   ),
   Projeto(
-    nome: "Aplicativo para PetShop- Flutter",
-    data: "Agosto 2024",
-    descricao: "Desenvolvimento em Dart, usando a framework Flutter,",
+    nome: "OhMyDog - Flutter",
+    descricao:
+        '''É um aplicativo para petshop desenvolvido em Flutter, utilizando Modular e MobX. Facilita agendamentos e
+gerenciamento de serviços para pets, proporcionando uma experiência integrada e eficiente para os clientes.\n
+              - Permite que os clientes agendem serviços para seus pets;\n
+              - Facilita o gerenciamento dos diversos serviços oferecidos pelo petshop;\n
+              - Proporciona uma experiência integrada e eficiente para os clientes;\n''',
     githubLink: "https://github.com/nrbedin/ohmydog",
   ),
   Projeto(
-    nome: "Aplicativo Portfólio - Flutter",
-    data: "Outubro de 2024",
-    descricao: "Desenvolvimento em Dart, usando a framework Flutter,",
-    githubLink: "https://github.com/nrbedin",
+    nome: "Portfólio - Flutter WEB",
+    descricao:
+        '''Este é o portfólio de Naiara Rafaela Bedin, graduada em Ciência da Computação, com mais de uma década de experiência em TI.
+           Aqui, você encontrará detalhes sobre minhas habilidades, experiências e alguns dos projetos que desenvolvi.\n
+              - Exibir minhas habilidades técnicas e interpessoais;\n
+              - Detalhar minhas experiências profissionais;\n
+              - Apresentar projetos desenvolvidos, com exemplos práticos;\n''',
+    githubLink: "https://github.com/nrbedin/nrbedin.github.io",
+  ),
+  Projeto(
+    nome: "Loja Virtual - Flutter",
+    descricao:
+        '''É uma plataforma de comércio eletrônico desenvolvida para facilitar pedidos de comida, incluindo pizzas, doces e lanches.
+           Através de um sistema de login, os usuários podem acessar uma lista de produtos, adicionar itens ao carrinho de compras, e finalizar pedidos de forma prática e rápida.\n
+              - Permite que os usuários se registrem e façam login para acessar a loja;\n
+              - Exibe uma lista de produtos disponíveis, permitindo aos usuários adicionar itens ao carrinho;\n
+              - Os usuários podem adicionar, remover e ajustar a quantidade de produtos no carrinho;\n
+              - Facilita o processo de conclusão do pedido;\n
+              - Os usuários podem consultar o status de entrega dos pedidos realizados;\n''',
+    githubLink: "https://github.com/nrbedin/lojaVirtual",
   ),
 ];
